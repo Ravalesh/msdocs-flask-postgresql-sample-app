e76cbe0952c5cac857807b9b183c019141fe855f
Ravalesh Bhat <ravaleshb@gmail.com>
2024-01-04 22:50:38 +0530

    Fixing the connection string extraction

--- azureproject/production.py
+++ azureproject/production.py
@@ -3,7 +3,7 @@ import os
 # Configure Postgres database based on connection string of the libpq Keyword/Value form
 # https://www.postgresql.org/docs/current/libpq-connect.html#LIBPQ-CONNSTRING
 conn_str = os.environ['POSTGRESQLCONNSTR_AZURE_POSTGRESQL_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str[:-1].split(';')}
 
 DATABASE_URI = 'postgresql+psycopg2://{dbuser}:{dbpass}@{dbhost}/{dbname}'.format(
     dbuser=conn_str_params['User Id'],
